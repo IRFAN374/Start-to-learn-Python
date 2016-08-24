3ddee3600191363f57670a0089a20e8d7dde5158
IRFAN374 <Irfanmd2503@gmail.com>
2016-08-24 22:20:36 +0530

    Update first.py
    find the maximum of 3 numbers..

--- first.py
+++ first.py
@@ -1,6 +1,23 @@
+
+/* this is my first program in the python
+this program works for find the maximum of 3 numbers:
+  Let's see how it works;
+*/
+
+
+
+
+/* here we take from user input x    */
 x=int (input("enter the value of x:"))
+
+/* here we take from user input y    */
 y=int (input("enter the value of y:"))
+
+/* here we take from user input z    */
 z=int (input("enter the value of z:"))
+
+
 print("Result of max 3 numbers")
+/* here printint the max of 3 numbers */
 print(max(x,y,z))
 input("Please press enter to exit") 
