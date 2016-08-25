88fcde8f739b70475f40ddd996964b7bb2968c37
IRFAN374 <Irfanmd2503@gmail.com>
2016-08-26 00:30:06 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,7 @@
+# This is my second program in the python..
+# I am trying to learn python in the easy way: and I am beginer on the GitHUb ..
+
+#  Please help and guided me to improve myself and so i will conribute on the Open Source project::
+#  This program works for if-else condition
+
+x=int(input("Enter the value of X:"))
